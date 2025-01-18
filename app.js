e9a18124bfdec91c1a527d488b354406c908f29f
dbagheri1986 <d.bagheri1986@gmail.com>
2025-01-18 23:47:55 +0100

    Add files via upload
    samaneh code

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const appDiv = document.getElementById('app');
+    appDiv.innerHTML += '<p>This is a simple JavaScript application!</p>';
+});
+
